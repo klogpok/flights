98adbc621d6966926e5029bc6d2cf89bc2eee01c
klogpok <kloleg@gmail.com>
2019-04-09 18:20:53 +0300

    Added toggle section functional

--- public/app.js
+++ public/app.js
@@ -1,36 +1,39 @@
-function toggle(type) {
-    switch (type) {
-        case 'arri':
-            document.querySelector('#dep-section').classList.add('hidden');
-            document.querySelector('#arri-section').classList.remove('hidden');
-            break;
-
-        case 'dep':
-            document.querySelector('#arri-section').classList.add('hidden');
-            document.querySelector('#dep-section').classList.remove('hidden');
-            break;
-    }
-}
-
-const elTableArrivals = document.querySelector('#arrivalTable');
-const elTableDeparts = document.querySelector('#departsTable');
 const elTableArrivalsBody = document.querySelector('#arrivalTableBody');
 const elTableDepartsBody = document.querySelector('#departsTableBody');
 const thFlightArrival = document.querySelectorAll('#arrivalTableHead th');
+const thFlightDeparts = document.querySelectorAll('#departTableHead th');
+const toggleSectionButtons = document.querySelectorAll('.toggleSection');
+const arrivalsSection = document.querySelector('#arrivals');
+const departsSection = document.querySelector('#departures');
 
-//const thFlightDeparts = document.querySelectorAll('.thFlight');
 let elString = '';
 let keySort = 'arrival';
 let flights = [];
 
+const toggleTables = () => {
+    [...toggleSectionButtons].forEach(button => {
+        button.addEventListener('click', event => {
+            if (event.target.textContent === 'Arrivals') {
+                arrivalsSection.classList.remove('hidden');
+                departsSection.classList.add('hidden');
+                init(elTableArrivalsBody, thFlightArrival, 'from');
+            } else {
+                arrivalsSection.classList.add('hidden');
+                departsSection.classList.remove('hidden');
+                init(elTableDepartsBody, thFlightDeparts, 'to');
+            }
+        });
+    });
+};
+
 // Order functionality event
-const sortEventHandler = (table, thFlight) => {
+const sortEventHandler = (table, thFlight, destination) => {
     [...thFlight].forEach(flight => {
         flight.addEventListener('click', function() {
             if (this.tagName === 'TH') {
                 removeActive(thFlight, this);
                 flights = sortByKey(flights, this.dataset.sortexpression, toggleOrder(this));
-                renderTable(table, flights);
+                renderTable(table, flights, destination);
             }
         });
     });
@@ -66,28 +69,28 @@ const getFlights = (table, destination) => {
         .then(data => data.json())
         .then(res => {
             if (res.length) {
-                if (destination === 'Tel-Aviv') {
-                    flights = res.filter(
-                        flight => flight.to.toLowerCase() === destination.toLowerCase()
-                    );
+                if (destination === 'from') {
+                    flights = res.filter(flight => flight.to.toLowerCase() === 'tel-aviv');
                 } else {
                     flights = res.filter(flight => flight.to.toLowerCase() !== 'tel-aviv');
                 }
                 sortedRes = sortByKey(flights, keySort);
-                renderTable(table, sortedRes);
+                renderTable(table, sortedRes, destination);
             }
         });
 };
 
-const renderTable = (table, flights) => {
+const renderTable = (table, flights, destination) => {
     removeFlights(table);
     elString = '';
+
     flights.forEach(flight => {
+        let toFrom = flight[`${destination}`];
         elString += `
         <tr>
           <td>${flight.by}</td>
           <td>${flight.id}</td>
-          <td>${flight.from}</td>
+          <td>${toFrom}</td>
           <td>${flight.arrival}</td>
           <td>${flight.terminal}</td>
         </tr>
@@ -107,9 +110,10 @@ const sortByKey = (obj, key, order = 'ASC') => {
         : obj.sort((a, b) => (a[key] < b[key] ? 1 : b[key] < a[key] ? -1 : 0));
 };
 
-const init = () => {
-    sortEventHandler(elTableArrivalsBody, thFlightArrival);
-    getFlights(elTableArrivalsBody, 'Tel-Aviv');
+const init = (tableBody, thFlight, destination) => {
+    sortEventHandler(tableBody, thFlight, destination);
+    getFlights(tableBody, destination);
+    toggleTables();
 };
 
-init();
+init(elTableArrivalsBody, thFlightArrival, 'from');
